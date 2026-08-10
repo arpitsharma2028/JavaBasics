66784f2942ab5d0940a458fac3faec349e063970
arpitsharma2028 <arpitsharma6105@gmail.com>
2026-08-10 13:57:45 +0530

    variables

---
+++ basics.java
@@ -0,0 +1,41 @@
+public class basics{
+    public static void main(String str[]){
+        System.out.println("Hello");
+
+        for(int i = 0 ; i <= 4 ; i++){
+            for(int j = 0 ; j < i ; j++){
+                System.out.print('*');
+            }
+            System.out.print("\n");
+        }
+
+        /*
+            typed language -  i have to tell language that this is type of data
+            
+            Primitive
+
+            byte
+            short
+            char
+            boolean
+            int
+            long
+            float
+            double
+
+        Non Primitive:
+
+            String
+            Array
+            Class
+            Object
+            Interface
+        */
+       // boolean a = 1; // error
+       boolean b = true;
+       
+
+
+
+    }
+}
